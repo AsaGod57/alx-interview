77bb3407101f409af7f44f4f17ec7d9aa2c9b4dc
Godsway Asamoah <gasamoah@wacci.ug.edu.gh>
2024-09-13 00:13:36 +0000

    My task0 rotate 2d matrix commit

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Function to rotate a 2D matrix by 90 degrees clockwise...
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotate a given n x n 2D matrix by 90 degrees clockwise in place...
+
+    Args:
+        2D matrix of integers to be rotated...
+
+    Returns:
+        Matrix...
+    """
+
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i, n):
+            # Swap elements across the diagonal
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for i in range(n):
+        matrix[i].reverse()
